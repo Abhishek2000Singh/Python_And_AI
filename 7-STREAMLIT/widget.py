c93b510025ea30869315dfa22b1d6227c48e9f35
Abhishek2000Singh <singhabhishek20200696@gmail.com>
2026-09-15 00:04:55 +0530

    nlp basics

--- 7-STREAMLIT/widget.py
+++ 7-STREAMLIT/widget.py
@@ -16,3 +16,19 @@ options = ["Python","Java","C++","JavaScript","Go","Ruby"]
 choice = st.selectbox("Choose Your Fav Language: ",options)
 st.write(f"Your selected option is:- {choice}")
 
+
+data ={
+    "Name":["John","Jane","Jake","Jill"],
+    "Age":[23,23,25,30],
+    "City":["Kolkata","Delhi","Gurugram","Pune"]
+}
+df = pd.DataFrame(data)
+df.to_csv("Sample.csv")
+st.write(df)
+
+upload_file = st.file_uploader("choose your csv or txt",type="csv")
+
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.write(df)
+
---
+++ 8-NLP/8.1-Basic.py
@@ -0,0 +1,18 @@
+#  Tokenization in NLP -> We take Paragraph, Sentences etc. to convert it into sentenes(TOken)
+
+# Topics - 
+
+# Corpus -> Paragraph
+# Documents -> Sentences
+# Vocabulary-> Unique Words
+# Words -
+
+## My name is Abhi and i love a internet exploring techs and gadget. I am a Genius
+## PErforming Tokenization
+## 1- My name is Abhi and i love a internet exploring techs and gadget 
+## 2- I am a Genius
+
+## I like to Drink Coffee . My fav food is Biryani.
+##        || Tokenization Process
+## My fav drink is Coffee  ==> Unique words -->  drink Coffee 
+## My fav food is Biryani ==> Unqiue Words -->  Food Biryani
